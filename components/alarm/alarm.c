464ac97a923a7fe331656b5d88d00819b9574ecd
Fabjuuuh <f.degroot1@student.avans.nl>
2021-03-19 09:52:42 +0100

    .

---
+++ components/alarm/alarm.c
@@ -0,0 +1,32 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "clock-sync.h"
+
+#include "esp_log.h"
+
+#define ALARMTAG "alarm"
+
+struct alarmtime{
+    char** songs;
+    int* time;
+};
+
+
+void alarm_task(void*pvParameter){
+
+    struct alarmtime alarmtimes[10];
+
+    alarmtimes[0].songs = NULL;
+    alarmtimes[0].time = clock_getCurrentTime();
+    while (1)
+    {
+        for(int i=0; i<sizeof(alarmtimes)/sizeof(alarmtimes[0]); i++){
+          if(alarmtimes[i].time == clock_getCurrentTime()){
+               ESP_LOGI(ALARMTAG, "alarm going off");
+          }
+        }
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        
+    }
+    
+}
---
+++ components/alarm/include/alarm.h
@@ -0,0 +1,6 @@
+#ifndef alarm_H
+#define alarm_H
+
+void alarm_task(void*pvParameter);
+
+#endif
