b2b1eee0bea2bb679bfb9f8743542e474aa0acd8
fabjuuuh <f.degroot1@student.avans.nl>
2021-03-22 14:02:48 +0100

    Comments to alarm

--- components/alarm/alarm.c
+++ components/alarm/alarm.c
@@ -8,6 +8,9 @@
 
 #define ALARMTAG "alarm"
 
+//Struct for all the alarm nodes
+//songs is what will be playes when the alarm goes off
+// time is the time when the alarm goes off
 struct ALARM{
     char** songs;
     int* time;
@@ -18,6 +21,7 @@ typedef struct ALARM *Node;
 
 Node head, node;
 
+//Creates an alarm node 
 Node CreateAlarm(int* time, char** songs)
 {
     struct ALARM *alarm;
@@ -32,17 +36,20 @@ Node CreateAlarm(int* time, char** songs)
     return alarm;
 };
 
+//Free the node
 void FreeNode(Node node){
     if(node){
         free(node);
     }
 }
 
+//Place the node at the beginning of the list
 void Prepend(Node *head, Node node){
     node->next = *head;
     *head = node;
 }
 
+//Place the node at the end of the list
 void Append(Node *head, Node node){
     Node tmp = *head;
     if(*head == NULL){
@@ -56,6 +63,7 @@ void Append(Node *head, Node node){
     }
 }
 
+//Removes a node
 Node Remove(Node *head, Node node){
     Node tmp = *head;
     if(*head == NULL){
@@ -75,6 +83,7 @@ Node Remove(Node *head, Node node){
     }
 }
 
+//Clear the list
 void Clear(Node head){
     Node node;
 
@@ -85,6 +94,7 @@ void Clear(Node head){
     }
 }
 
+//Print a node
 void Print(Node node){
     if(node){
         int* time = node->time;
@@ -92,6 +102,7 @@ void Print(Node node){
     }
 }
 
+//Print the entire list
 void PrintList(Node head){
     while(head){
         Print(head);
@@ -99,15 +110,19 @@ void PrintList(Node head){
     }
 }
 
+
 void alarm_task(void*pvParameter){
     head = NULL;
 
     while(1){
         if(clock_getCurrentTime() != NULL){
-            int* current = clock_getCurrentTime();
+            int* current = clock_getCurrentTime(); //Update current time
             Node tmp = head;
             while(tmp){
                 int *alarmTime = tmp->time;
+                //When the hour and minute are the same the alarm should go off 
+                //alarmTime[0] = hour
+                //alarmTime[1] = minute
                 if(alarmTime[0] == current[0] && alarmTime[1] == current[1]){
                     ESP_LOGI(ALARMTAG, "Alarm going off %d, %d", alarmTime[1], current[1]);
                     //Do stuff when alarm goes off
@@ -124,6 +139,7 @@ void alarm_task(void*pvParameter){
     
 }
 
+//Method to add an alarm node to the list
 void alarm_add(int* time, char** songs){
     Node newNode = CreateAlarm(time, songs);
     Prepend(&head, newNode);
@@ -131,5 +147,5 @@ void alarm_add(int* time, char** songs){
 }
 
 void alarm_going(){
-
+    //TODO play music from sd
 }
